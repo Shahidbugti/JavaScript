8ba40a78105e2b08150a65562f3065bcaa18961d
Shahid <bugtimuhammadshahid1@gmail.com>
2023-08-21 18:04:27 +0500

    Operators

---
+++ operators/operator.js
@@ -0,0 +1,32 @@
+// basic operators
+
+// Addition
+let num1 = 10;
+let num2 = 5;
+// console.log(num1 + num2);
+
+//subtraction
+// console.log(num1 - num2);
+
+//Multiplication
+// console.log(num1 * num2);
+
+//devision
+// console.log(num1/num2);
+
+//remander
+// console.log(num1%num2);
+//exponential
+// console.log(num1**num2);
+
+// increament
+// num1++
+// console.log(num1);
+
+//decrement
+// num2--
+// console.log(num2);
+
+// num1 = num1 + 20;
+num1+=20;
+console.log(num1);
