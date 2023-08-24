7122a241ed817e348fcbbb18f841d4346881302e
Shahid <bugtimuhammadshahid1@gmail.com>
2023-08-24 18:50:48 +0500

    ...

--- Overview/Function.js
+++ Overview/Function.js
@@ -1,34 +1,34 @@
-// function greet(){
-//     console.log("Welcome to our Website " + name);//I cant access name here because i havent passed the arrgument.                                                 //THIS CONCEPT IS KNOWN AS "LEXICAL SCOPING" OR "FUNCTION SCOPE"
-//  }
+function greet(){
+    console.log("Welcome to our Website " + name);//I cant access name here because i havent passed the arrgument.                                                 //THIS CONCEPT IS KNOWN AS "LEXICAL SCOPING" OR "FUNCTION SCOPE"
+ }
  
-//  console.log("hello");
+ console.log("hello");
  
-//  function signUp(){
-//      let name = prompt("Enter your name");
-//      greet();
-//  }
+ function signUp(){
+     let name = prompt("Enter your name");
+     greet();
+ }
  
-//  signUp();
+ signUp();
 
 
 //Here i can access name because i have passed  arrgument
 
-// function greet(name){
-//    console.log("Welcome to our Website " + name);
-// }
-// console.log("hello");
+function greet(name){
+   console.log("Welcome to our Website " + name);
+}
+console.log("hello");
 
-// function signUp(){
-//     let name = prompt("Enter your name");
-//     greet(name);
-// }
+function signUp(){
+    let name = prompt("Enter your name");
+    greet(name);
+}
 
-// signUp();
-
-
+signUp();
 
 
+
+//This function compares two numbers and return the greater number
 function max(nr1, nr2){
     if(nr1 > nr2){
         return nr1;
---
+++ Overview/Object.js
@@ -0,0 +1,33 @@
+const user = {
+    name: "Shahid",
+    age : 18,
+    married: false,
+    greet: function(){
+        console.log("Hello there");
+    }
+}
+
+console.log(user.age);
+user.greet();
+
+//PRACTICE OF OBJECTS
+
+// Creating a dictionary object
+const dictionary = {
+    ardent: "Characterized by intense enthusiasm or passion for something.",
+    quixotic: "Extremely idealistic, unrealistic, and impractical.",
+    serendipity: "The occurrence of fortunate events by chance in a happy or beneficial way.",
+    mellifluous: "Sweet-sounding, smooth, and pleasing to the ear.",
+    ephemeral: "Lasting for a very short time; transient.",
+    ubiquitous: "Present, appearing, or found everywhere.",
+    voracious: "Having a very eager or insatiable appetite for something, often used to describe a strong desire for knowledge or food.",
+    pernicious: "Having a harmful effect, especially in a gradual or subtle way.",
+    obfuscate: "To make something unclear, confusing, or difficult to understand.",
+    sycophant: "A person who acts obsequiously towards someone important in order to gain advantage or favor; a flatterer."
+  };
+  
+  // Access word definitions
+ console.log(dictionary.ardent);
+ console.log(dictionary.sycophant);
+ console.log(dictionary.voracious);
+ console.log(dictionary.serendipity);
