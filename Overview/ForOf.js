bdf38f32d7e179a420e5f46ffa70460cffd7d814
Shahid <bugtimuhammadshahid1@gmail.com>
2023-08-26 23:45:49 +0500

    for-of and for-Each loops

---
+++ Overview/ForOf.js
@@ -0,0 +1,24 @@
+//FOR--OF LOOPS
+//genral form of for-of loops
+// for (let variable for iterable){
+//     code  to be excuted
+// }
+
+const names = ["Shahid", "Zahid", "Irfan", "Noman", "Awais", "Saad"];
+
+// for (let name of names) {
+//     console.log(names.indexOf(name)); // Print the index of the current name in the array
+//     console.log(name); // Print the current name
+// }
+
+for (let name of names){
+    if(name === "Irfan"){
+    break;
+}
+console.log(name);
+}
+
+//ForEach loops
+// names.forEach(function(name,index){
+//     console.log(name ,index)
+// })
