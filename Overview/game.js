d443a18c5cf78bb06f46f03bae4d1dde021db25b
Shahid <bugtimuhammadshahid1@gmail.com>
2023-08-27 18:44:32 +0500

    game to make User guess a number

---
+++ Overview/game.js
@@ -0,0 +1,53 @@
+//Genrate a random number
+//Give the user the ability to guess
+//if they are wrong, ask them again
+//If they win congratulate them
+
+// function guessGame(){
+//    let randomNr = Math.floor(Math.random()*10);
+//    let guess;
+
+//    do{
+//     guess = prompt("Guess a number between 1 - 10");
+//     console.log(guess , randomNr);
+//     if(randomNr > guess){
+//         console.log("You guessed too low");
+//     }
+//     else if(randomNr < guess){
+//         console.log("you gussed too high");
+//     }
+//    }
+//    while(guess != randomNr);
+//    console.log("congratulations you won");
+// }
+// guessGame();
+
+
+function guessGame(){
+    let randomNr = Math.floor(Math.random()*10 + 1);
+    let guess;
+    // let attempts = 0;
+
+
+    while(true){
+        guess = prompt("Guess a number a b/w 1 - 10");
+        guess = parseInt(guess);
+
+        if(isNaN(guess)){
+            console.log("Please enter a valid number");
+        }
+        // else{
+        //     attempts++;
+        // }
+ if(randomNr > guess){
+     console.log("guess is lower");
+        } else if(randomNr < guess){
+            console.log("guess is higher");
+        }
+        else{
+            console.log("COngratulations, You won in ${attemps} attempts!");
+            break;
+        }
+    }
+}
+guessGame();
