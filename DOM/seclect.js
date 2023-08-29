850fff953055ece2ad96290feb46f38c1687f00d
Shahid <bugtimuhammadshahid1@gmail.com>
2023-08-29 20:32:01 +0500

    Started learning DOM

---
+++ DOM/seclect.js
@@ -0,0 +1,15 @@
+// const header= document.getElementsByTagName("h2");
+// const list = document.getElementsByClassName("item");
+// const button = document.getElementById("submit")
+// console.log(header);
+// console.log(list);
+// console.log(list.length);
+// console.log(button);
+
+// const headers =  document.querySelector("h2");//=>Return the first h2 in the page 
+// console.log(headers);
+const heading = document.querySelectorAll("h2");//=>Return the all h2 in the page
+console.log(heading);
+
+const ul = document.querySelector("#list");
+console.log(ul);
