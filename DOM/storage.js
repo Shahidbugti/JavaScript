8fcbb9597b6ecd3459d2e862b68ae5d31b5d42b3
Shahid <bugtimuhammadshahid1@gmail.com>
2023-09-07 19:34:28 +0500

    learnt storage in Js

--- DOM/storage.js
+++ DOM/storage.js
@@ -1,18 +1,17 @@
 //LOCAL STORAGE
 // localStorage.setItem("Name", "Shahid");
 
-const key = prompt("Enter the key you want to set");
-const value = prompt("Enter the value you want to set");
-localStorage.setItem(key,value);
-console.log(`The value at ${key} is ${localStorage.getItem(key)}`);
+// const key = prompt("Enter the key you want to set");
+// const value = prompt("Enter the value you want to set");
+// localStorage.setItem(key,value);
+// console.log(`The value at ${key} is ${localStorage.getItem(key)}`);
+// if(key == 0){
+//     localStorage.removeItem("key");
+// }
 
-console.log(localStorage.length)
+// if(value == 0){
+// localStorage.clear();
+// }
 
-if(key == 0){
-    localStorage.removeItem("key");
-}
-
-if(value == 0){
-localStorage.clear();
-}
-
+sessionStorage.setItem("name","Shahid");
+sessionStorage.removeItem("key");
