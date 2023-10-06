44e8f842ee78bdbe92444f75b78f6d45f6737b6b
Shahid <bugtimuhammadshahid1@gmail.com>
2023-10-06 07:59:35 +0500

    Veriables

--- variable/variable.js
+++ variable/variable.js
@@ -1,12 +1,27 @@
-let class1 = 15;
-let class2 = 25;
- let class3 = class1+class2;
- console.log (class3);
+//VAR==>veriable defined with var are reasigned and redecleared
+// var a = 10;
+// var a = 15;
+// console.log(a);//output is 15
 
 
-const name =  "shahid";
-console.log(name);
+
+//HOISTING ==> hoisting is a machenism where variable are moved to top of there scope before excution of code
+console.log(greeter);
+var greeter = "say hello"
+
+
+// The variable functionally scoped defined with var effects the globaly defined variable
+// var greeter = "hello";
+// if(true){
+//     var greeter = "hello instead";
+// }
+// console.log(greeter);
+// let class1 = 15;
+// let class2 = 25;
+//  let class3 = class1+class2;
+//  console.log (class3);
+
 
 // camel case 
-const firstName = "shahid";
-const lastName = "bugti";
+// const firstName = "shahid";
+// const lastName = "bugti";
