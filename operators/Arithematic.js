ff5741e167e7551fa70a36935004f124ba96e87d
Shahid <bugtimuhammadshahid1@gmail.com>
2023-10-08 22:59:05 +0500

    Arithematic Operators

---
+++ operators/Arithematic.js
@@ -0,0 +1,57 @@
+//The arithmetic operators are used to perform mathematical operations like addition and subtraction
+//EXAMPLE
+let x = 3;
+let y = 8;
+console.log(x + y);
+
+//TYPES  OF ARITHEMATIC OPERATORS  ===> there are 8 types of Arithematic operators
+// Addition +
+// Subtraction -
+// Multiplication *
+// Division /
+// Remainder %
+// Exponentiation **
+// Increment ++
+// Decrement --
+
+//  1-) ADDITION
+let a = 3;
+let b = 8;
+console.log(a + b);
+
+// 2-) SUBTRACTION
+let console = 5;
+let d = 3;
+console.log(c - d);
+
+// 3-) MULTIPLICATION  (represented by asterisk *);
+console.log(5 * 2); //10
+
+// 4-) DEVISION
+console.log(10 / 2);
+
+
+//5-)REMAINDER (Modulus Operators)
+console.log(10 % 2); //1
+
+
+//6-) EXPONENTION OPERATOR
+console.log(10 ** 3); // 1000
+
+
+// 7-) INCREMENT OPERATORlet x = 5;
+let w = 5;
+w++;
+console.log(w) ;
+ //or
+ //let w = 5;
+ //w = w + 1;
+ //console.log(w);
+
+ 
+ // Place the operator next to the variable (postfix)
+x++;
+
+// Place the operator before the variable (prefix)
+++x;
+
