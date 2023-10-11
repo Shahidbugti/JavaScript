e2dc82b31b12507cf0249323f370c65b4de699d8
Shahid <bugtimuhammadshahid1@gmail.com>
2023-10-11 23:33:05 +0500

    operators

---
+++ operators/comparison.js
@@ -0,0 +1,12 @@
+//COMPARISON OPERATORS *Used to compare values or variables
+// Always return boolean
+
+// NAME             	   OPERATION EXAMPLE	   MEANING
+// Equal	                  x == y	      Returns true if the operands are equal
+// Not equal	              x != y	      Returns true if the operands are not equal
+// Strict equal	              x === y	      Returns true if the operands are equal and have the same type
+// Strict not equal	          x !== y	      Returns true if the operands are not equal, or have different types
+// Greater than	              x > y	          Returns true if the left operand is greater than the right operand
+// Greater than or equal      x >= y	      Returns true if the left operand is greater than or equal to the right operand
+// Less than	              x < y	          Returns true if the left operand is less than the right operand
+// Less than or equal	     x <= y	          Returns true if the left operand is less than or equal to the right operand
---
+++ operators/logical.js
@@ -0,0 +1,31 @@
+// LOGICAL OPERATORS ===> Logical operators are used to check whether one or more expressions result in either true or false.
+
+//  Can only return booleon values
+
+// There are three logical operators in js
+
+// 1-) Logical AND (&&)
+// It returns true if both oprerands are true otherwise false
+console.log(7 > 2 && 2 > 1);//True
+
+
+let a = 5;
+let b;
+if(a && b){
+  console.log("Yes")
+}
+else{
+  console.log("false")
+} //FALSE
+
+//Logical OR (||)
+//It returns if atleast one of Operands is true otherwise false
+let c = 5;
+let d;
+if(c && d){
+  console.log("Yes")
+}
+else{
+  console.log("false")
+}
+//true
