4de20bc57259aedd488489fc8f38157476033b73
Shahid <bugtimuhammadshahid1@gmail.com>
2023-10-16 16:19:44 +0500

    Ternary Operators

---
+++ operators/ternary.js
@@ -0,0 +1,20 @@
+//TERNARY OPERATORS (Also called conditional operators).
+//require three operands to run
+
+
+//SYNTAX
+
+//condition ? expression1 : expression2,
+let totalPurchase = 15;
+let discount = totalPurchase >=20 ? 3 : 1;
+// Above code means
+
+// let totalPurchase = 15;
+
+// let discount;
+
+// if (totalPurchase >= 20) {
+//   discount = 3;
+// } else {
+//   discount = 1;
+// }
